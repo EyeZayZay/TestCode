01360b3864d0aa4760e6f21278287d7a9a1e6ab3
EyeZayZay <admin@isaiahmiddleton.com>
2015-07-14 11:46:35 -0700

    Update cameras.py

--- cameras.py
+++ cameras.py
@@ -1,4 +1,7 @@
-# The idea is to open the file, go through each line, add the camera to a list only if the list doesn't already contain the camera. Once that's complete, we create a new file, and add each camera from the list to this newly created file.
+# The idea is to open the file, go through each line, 
+# add the camera to a list only if the list doesn't already contain the camera. 
+# Once that's complete, we create a new file, 
+# and add each camera from the list to this newly created file.
 
 f = open(“camera_list.txt”, “r”)
 cameras = []
